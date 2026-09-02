bdadbcb435f474fabe986f490cdf2b63deebb48d
Fateme Fazel <fatemefazelzade30@gmail.com>
2026-09-02 02:25:00 +0200

    Form submit

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+
 function AddNotes() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newNote = {
+      title,
+      description,
+      id: Date.now,
+      completed: false,
+      createdAt: new Date().toISOString,
+    };
+    setTitle("");
+    setDescription("");
+  };
+
   return (
     <div className="grid-3">
-      <form className="search" action="" method="get">
+      <form className="search" onSubmit={handleSubmit}>
         <h2>Add new notes</h2>
         <input
           type="text"
-          placeholder="Write your headline"
-          className="search__input headline"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Write your title"
+          className="search__input title"
         />
         <input
           type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
           placeholder="Write your description"
           className="search__input body"
         />
-        <input type="submit" value="Add!" className="search__button" />
+        <button type="submit" className="search__button">
+          Add note
+        </button>
       </form>
     </div>
   );
